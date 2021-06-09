595adaaa478ba3ff0c3af814b1ffe98a186d3d14
prahal <vprahaladhan@gmail.com>
2021-06-09 14:48:00 +1000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :name, presence: true, uniqueness: true
+  validates_with PhoneValidator
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with PostValidator
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category!" }
+  validate :title_not_sufficiently_clickbaity
+
+  def title_not_sufficiently_clickbaity
+    clickbaity_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    if !( clickbaity_titles.any? { |t| title.to_s.include?(t) } ) then
+      errors.add(:title, "is not sufficiently clickbaity!")
+    end
+  end
 end
